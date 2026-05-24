75c22f59e43c20be2af1b0dee42b84b3a4099eee
a.glotov <andruxa.comm@gmail.com>
2026-05-24 23:57:24 +0500

    feat: 04-basic-auth-guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 import { appConfig } from './config/app.config';
 import { databaseConfig } from './config/database.config';
 import { DatabaseModule } from './database/database.module';
@@ -17,6 +18,7 @@ import { ObservabilityModule } from './observability/observability.module';
     }),
     ObservabilityModule,
     ErrorsModule,
+    AuthModule,
     DatabaseModule,
   ],
   controllers: [AppController],
